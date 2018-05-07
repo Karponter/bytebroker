8e51fac5d6077108c7b6cc5cda900efe7e9ced6f
Vlad Dotsenko <vlad.dotsenko@sigma.software>
2018-05-07 23:14:19 +0300

    dacs: add docs folder

--- test/Datasource.test.js
+++ test/Datasource.test.js
@@ -4,11 +4,16 @@ const testDatasourceIntegrity = require('./datasource-integrity-tester');
 
 const ireq = require('../ireq');
 const InMemoryDatasource = ireq.lib.datasource('./InMemoryDatasource');
+const JsonDatasource = ireq.lib.datasource('./JsonDatasource');
 
 const vendorProvidedDaasources = [
   {
     class: InMemoryDatasource,
     arguments: null,
+  },
+  {
+    class: JsonDatasource,
+    arguments: '/tmp/test.json',
   },
 ];
 
